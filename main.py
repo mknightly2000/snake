575f8c4818abacfe3eb83f25bdd49c9ab6d1b24e
Michael Knightly <99145919+mknightly2000@users.noreply.github.com>
2025-04-20 19:01:22 -0700

    feat: implement game board sizing
    The game screen can have a different size than the menu screen, as the
    number of cells in the game board can be changed. The number of cells is
    determined by the board dimensions.

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ import pygame
 
 
 class Game:
+
     def __init__(self):
-        self.screen_width = 350
-        self.screen_height = 500
-        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+        self.menu_screen_width = 350
+        self.menu_screen_height = 500
+
+        self.cell_size = 25  # the width and length of a cell in the board
+        self.board_dimensions = (14, 20)
+        self.game_screen_width = self.cell_size * self.board_dimensions[0]
+        self.game_screen_height = self.cell_size * self.board_dimensions[1]
 
         self.font_semi_bold = "fonts/PixelifySans-SemiBold.ttf"
         self.background_color = (64, 197, 91)
@@ -31,18 +36,20 @@ class Game:
         sys.exit()
 
     def menu(self):
+        screen = pygame.display.set_mode((self.menu_screen_width, self.menu_screen_height))
+
         font = pygame.font.Font(self.font_semi_bold, 35)
 
         menu_title = font.render("Main Menu", False, (0, 0, 0))
         play_btn = font.render("Play", False, (0, 0, 0))
         exit_btn = font.render("Exit", False, (0, 0, 0))
 
-        self.screen.fill(self.background_color)
+        screen.fill(self.background_color)
 
-        self.screen.blit(menu_title, (10, 5))
-        self.screen.blit(play_btn, (10, 80))
+        screen.blit(menu_title, (10, 5))
+        screen.blit(play_btn, (10, 80))
         play_btn_rect = play_btn.get_rect(topleft=(10, 80))
-        self.screen.blit(exit_btn, (10, 120))
+        screen.blit(exit_btn, (10, 120))
         exit_btn_rect = exit_btn.get_rect(topleft=(10, 120))
 
         pygame.display.update()
@@ -61,12 +68,14 @@ class Game:
                         self.exit_game()
 
     def game(self):
+        screen = pygame.display.set_mode((self.game_screen_width, self.game_screen_height))
         font = pygame.font.Font(self.font_semi_bold, 35)
+
         menu_txt = font.render("Game running...", False, (0, 0, 0))
 
-        self.screen.fill(self.background_color)
+        screen.fill(self.background_color)
 
-        self.screen.blit(menu_txt, (10, 5))
+        screen.blit(menu_txt, (10, 5))
 
         pygame.display.update()
 
